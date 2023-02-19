cb240ff70f5e4379f23b1ae4874e5558e6d5a2f0
derodero24 <k.itsh20524abc@gmail.com>
2023-02-19 13:22:06 +0900

    setup textarea UI

--- src/sections/TextArea.tsx
+++ src/sections/TextArea.tsx
@@ -1,3 +1,8 @@
 export default function TextArea() {
-  return <section className="h-12 bg-yellow-900">textarea</section>;
+  return (
+    <section className="flex h-12 items-center">
+      <p className="ml-12 mr-1 text-sm font-medium">Text:</p>
+      <textarea className="h-full grow resize-none bg-black/80 px-1 py-0.5 text-sm leading-[1.1] text-white" />
+    </section>
+  );
 }
